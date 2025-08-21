7761751bde1d3b67f617165378bdb9a586470b28
ahmedsanad <71617361+ahmedsanad666@users.noreply.github.com>
2025-08-21 04:40:56 +0300

    update docker file

--- obj/Debug/net9.0/BackEnd.AssemblyInfo.cs
+++ obj/Debug/net9.0/BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f511d29d6491f48e1228863fbaff17cb098f3f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4741a400cf4d0347ef63690bf65ab1921b8df35d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
